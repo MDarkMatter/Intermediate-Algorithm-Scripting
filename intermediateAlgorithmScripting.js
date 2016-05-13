3667baa0a3dd90a1dde46aeb221966ff6663f4ec
Matt <mdark.math@gmail.com>
2016-05-13 14:42:50 -0700

    problem 2 complete

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -5,11 +5,11 @@
 // The lowest number will not always come first.
 
 function sumAll(arr) {
-  var allDigits = [];
-   for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
-      allDigits.push(i);
-   }
-   return allDigits.reduce( (prev, curr) => prev + curr );
+    var allDigits = [];
+    for (var i = Math.min(...arr); i <= Math.max(...arr); i++) {
+        allDigits.push(i);
+    }
+    return allDigits.reduce((prev, curr) => prev + curr);
 }
 
 
@@ -19,9 +19,24 @@ function sumAll(arr) {
 // Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 
 function diffArray(arr1, arr2) {
-  var newArr = [];
-  // Same, same; but different.
-  return newArr;
+    var newArr = arr1.concat(arr2);
+    var nonDupe = newArr.filter(findDupes);
+    return nonDupe;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+function findDupes(element, index, array) {
+    var arrSlice = array.slice(0);
+    var removed = arrSlice.splice(index, 1);
+
+
+    if (arrSlice.indexOf(removed[0]) === -1) {
+        arrSlice.push(removed[0]);
+        return true;
+    } else {
+        arrSlice.push(removed[0]);
+        return false;
+
+
+    }
+
+}

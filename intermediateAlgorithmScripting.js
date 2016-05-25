6034d29d7bc7552e94fa0bb6d9768d1bf80aef57
Matt <mdark.math@gmail.com>
2016-05-25 10:51:50 -0700

    problem 5 done

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -201,3 +201,36 @@ function whereAreYou(collection, source) {
 
   return arr;
 }
+
+
+
+// Search and Replace
+
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+// First argument is the sentence to perform the search and replace on.
+
+// Second argument is the word that you will be replacing (before).
+
+// Third argument is what you will be replacing the second argument with (after).
+
+// Preserve the case of the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+
+function myReplace(str, before, after) {
+   var re = new RegExp(before, 'gi');
+   var newString = str.replace(re, firstChar(before,after));
+
+   return newString;
+}
+
+function firstChar(before, after){
+   console.log(before);
+   console.log(after);
+   if (before[0] === before[0].toUpperCase()){
+      var newAfter = after.split('');
+      newAfter.splice(0,1,newAfter[0].toUpperCase());
+      return newAfter.join('');
+   } else {
+      return after;
+   }
+}

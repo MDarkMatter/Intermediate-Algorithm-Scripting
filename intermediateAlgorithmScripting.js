332b6b39339d3d0b7a3d4b888e612df1c11ff79a
Matt <mdark.math@gmail.com>
2016-05-25 14:56:47 -0700

    problem 8 done

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -307,3 +307,18 @@ function pairElement(str) {
 
    return arr3;
 }
+
+
+// Missing letters
+
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+
+function fearNotLetter(str) {
+   for (var i = 0; i < str.length; i++){
+      if (str.charCodeAt(i)+1 !== str.charCodeAt(i+1) && isNaN(str.charCodeAt(i+1)) === false){
+         return String.fromCharCode(str.charCodeAt(i)+1);
+      }
+   }
+}
